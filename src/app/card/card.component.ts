3206d3082567a6bca7198beb4ad1726a50671883
yu060432 <1181868657@qq.com>
2020-03-04 16:49:16 +0800

    22

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -5,6 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
+
+
+
+
 export class CardComponent implements OnInit {
 
   totalminutes = 60;
